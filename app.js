b5f22860975302fbfd612747fc857046b3ad2a83
ZhangLynn <zl8312070@sina.com>
2019-11-27 10:43:33 +0800

    :fire: remove old router

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const userRouter = require('./routes/user');
 const blogRouter = require('./routes/blog')
 var app = express();
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/user', userRouter)
 app.use('/api/blog', blogRouter)
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,15 +1,27 @@
 /**
  * created by LynnZhang on 2019/11/27
  */
-/**
- * created by LynnZhang on 2019/11/27
- */
 var express = require('express');
 var router = express.Router();
 
-router.get('/login', function(req, res, next) {
-    const { username, password} = req.body;
-    res.send(username + password);
+router.get('/list', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/detail', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/new', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/update', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/delete', function(req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
